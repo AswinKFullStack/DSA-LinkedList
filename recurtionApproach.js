144d33509738bac3fe8e3aaf6f1d63add6e1b24a
Aswin K <aswinkplacement@gmail.com>
2024-11-06 16:25:51 +0530

    fibnocii implemented using recurtion

---
+++ recurtionApproach.js
@@ -0,0 +1,29 @@
+function fibonacci(n , currentPosition = 2 ,first = 0,second = 1){
+    if( n<0){
+        return null
+    }
+    if(n<2){
+        return n
+    }
+
+  
+    let curr =  first + second;
+    
+    if(currentPosition === n){
+        return curr
+
+    }
+   
+    return  fibonacci(n,currentPosition + 1,second,curr)
+}
+
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(6));
+console.log(fibonacci(7));
+console.log(fibonacci(8));
+console.log(fibonacci(9));
